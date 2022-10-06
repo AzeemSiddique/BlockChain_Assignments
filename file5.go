5f5ac004b36e9cd56ac62f42861a5e2144f6602b
AzeemSiddique <AzeemSiddique>
2022-10-06 21:39:45 +0500

    Assignment 1

--- file5.go
+++ file5.go
@@ -19,37 +19,37 @@ func NewBlock(num int, transaction string, nonc int, pre_h string) *Block {
 	s := new(Block)
 	s.blockno = num
 	s.transact = transaction
-	s.nonce = nonc
-	s.pre_hash = pre_h
-	non := strconv.Itoa(nonc)
-	var cal_hash string = transaction + non + pre_h
+	s.nonce = nonc			//nonce
+	s.pre_hash = pre_h		//previous hash
+	non := strconv.Itoa(nonc)		//nonce to string
+	var cal_hash string = transaction + non + pre_h		//calculate hash
 	s.hash = CalculateHash(cal_hash)
-	endhash=s.hash
+	endhash=s.hash		//end hash for chain verification
 
 	return s
 }
 func CalculateHash(ds string) string {
-	return fmt.Sprintf("%x", sha256.Sum256([]byte(ds)))
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(ds)))		//calculate hash using sha256
 }
 type BlocktList struct {
-	list[]*Block
+	list[]*Block		//list of blocks in blockchain
 }
 
 func (ls *BlocktList) Createblock (transaction string, nonc int)*Block{
-	length_of_list := len(ls.list)
+	length_of_list := len(ls.list)		//length of list of blocks
 	pre_hash := ""
 	if length_of_list > 0 {
-		pre_hash = ls.list[length_of_list-1].hash
+		pre_hash = ls.list[length_of_list-1].hash	//previous hash of last block
 	}
 
-	block := NewBlock( len(ls.list), transaction, nonc, pre_hash)
-	ls.list = append(ls.list, block)
+	block := NewBlock( len(ls.list), transaction, nonc, pre_hash)		//create new block with previous hash
+	ls.list = append(ls.list, block)		//append new block to list of blocks in blockchain
 	return block
 }
 func (ls *BlocktList) Print(){
 	length:=len(ls.list)
 	i:=0
-	for i < length {
+	for i < length {												//print all blocks in blockchain 
 		fmt.Println("\n\n\tBlock Number = ", ls.list[i].blockno)
 		fmt.Println("\tTransection  = ", ls.list[i].transact)
 		fmt.Println("\tNonce        = ", ls.list[i].nonce)
@@ -61,26 +61,26 @@ func (ls *BlocktList) Print(){
 func (ls *BlocktList) ChangeBlock(num int, transaction string) {
 	size := len(ls.list)
 
-	ls.list[num].transact = transaction
+	ls.list[num].transact = transaction		//change transaction of block number num
 
-	for i:=num;i<size;i++{	
+	for i:=num;i<size;i++{			//recalculate hash of all blocks after block number num 
 		t:=ls.list[i].transact
 		n:=ls.list[i].nonce
 		prevh:=ls.list[i-1].hash
 
-		non := strconv.Itoa(n)
-		var cal_hash string = t + non + prevh
-		ls.list[i].hash = CalculateHash(cal_hash)
+		non := strconv.Itoa(n)			//nonce to string 
+		var cal_hash string = t + non + prevh		//calculate hash 
+		ls.list[i].hash = CalculateHash(cal_hash)		//calculate hash using sha256
 	}
 
 
 }
 func (ls *BlocktList) Chain_Verification() {
 	i:=len(ls.list)-1
-	if ls.list[i].hash==endhash {
-		fmt.Println("\n\tChain Is Valid!")
+	if ls.list[i].hash==endhash {			//check if end hash is same as hash of last block 
+		fmt.Println("\n\tThis BlockChain Is Valid!")
 	} else {
-		fmt.Println("\n\tChain Is Not Valid!\n\tError in Block Number ", i)
+		fmt.Println("\n\tThis BlockChain Is Not Valid! \n\tError detected in Block Number ", i)
 	}
 
 }
@@ -88,14 +88,14 @@ func (ls *BlocktList) Chain_Verification() {
 // func (ls *StudentList) Print(){
 func main() {
 	student := new( BlocktList)
-	student.Createblock("Haji Ibrar 2 rr", 233)
-	student.Createblock ("Mola Jutt2 ss" , 432)
-	student.Createblock ("gd ss ss" , 56)
+	student.Createblock("This is transaction 1", 233)			//create block with transaction and nonce
+	student.Createblock ("Transaction 2 is proccessing" , 432)
+	student.Createblock ("Third no transaction" , 56)
 	student.Print()
 
-	student.ChangeBlock(1,"AHAHAHAHAH 2 PhukYuu")
+	student.ChangeBlock(1,"Change block")		//change transaction of block number 1
 	student.Print()
 
-	student.Chain_Verification()
+	student.Chain_Verification()		//verify blockchain 
 	
 }
